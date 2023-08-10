18964e6fb6c825f43275e8b31a1072e18a00afe6
hobbs-chris <hobbs.chris.m@gmail.com>
2023-08-10 12:58:21 -0500

    added get all and get isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,27 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  //Find books by author
+  const booksByAuthor = books.filters((book) => book.author === author);
+  if(booksByAuthor.length > 0) {
+    return res.send(booksByAuthor);
+  } else {
+    return res.status(404).send("Author not found");
+  }
 });
 
 // Get all books based on title
